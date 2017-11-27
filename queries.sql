a7ca0a318a64e5da08dbef4bb125ddc1430eb1f3
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2017-11-27 21:05:27 +0000

    Added query a)

--- queries.sql
+++ queries.sql
@@ -1,3 +1,31 @@
+--a) Qual o nome do fornecedor que forneceu o maior número de categorias?
+--   Note que pode ser mais do que um fornecedor.
+select *
+from (
+	select max(forn_num.cat_num) as cat_num
+	from(
+		select count(forn_cat.categoria) as cat_num
+		from (
+			select P.forn_primario as nif, P.categoria as categoria
+			from produto as P
+			union
+			select S.nif as nif, P.categoria as categoria
+			from fornece_sec as S natural join produto as P
+		) as forn_cat
+		group by forn_cat.nif
+	) as forn_num
+) as max_num natural join (
+							select forn_cat.nif, count(forn_cat.categoria) as cat_num
+							from (
+								select P.forn_primario as nif, P.categoria as categoria
+								from produto as P
+								union
+								select S.nif as nif, P.categoria as categoria
+								from fornece_sec as S natural join produto as P
+							) as forn_cat
+							group by forn_cat.nif
+			) as produtos
+
 --c) Quais os produtos (ean) que nunca foram repostos
 (select ean from produto)
   except
@@ -8,3 +36,4 @@ select distinct ean
 from fornece_sec
 group by ean
 having count(nif) > 10;
+
