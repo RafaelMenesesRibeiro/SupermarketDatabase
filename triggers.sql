e5f508eb87a9982e98164f62c366686bf642b2b7
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2017-12-10 10:03:08 +0000

    Added triggers

---
+++ triggers.sql
@@ -0,0 +1,37 @@
+-- a) O fornecedor (primário) de um produto não pode existir na relação 
+--	  fornece_sec para o mesmo produto.
+CREATE OR REPLACE FUNCTION do_produto() RETURNS trigger as $$
+	begin
+		if new.forn_primario in (
+			select nif
+			from fornece_sec
+			where ean = new.ean
+		) then
+			RAISE EXCEPTION 'The primary supplier given is already a secondary supplier for this product. This cannot happen.';
+		end if;
+
+		return NEW;
+	end;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER chk_produto
+	before insert or update on produto
+	FOR each row execute procedure do_produto()
+;
+
+-- b) O instante mais recente de reposição tem de ser sempre anterior ou igual 
+--	  à data atual.
+CREATE OR REPLACE FUNCTION do_reposicao() RETURNS trigger as $$
+	begin
+		if new.instante > current_timestamp then
+			RAISE EXCEPTION 'The restocking instant given is in the future. This cannot happen.';
+		end if;
+
+		return NEW;
+	end;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER chk_reposicao
+	before insert or update on reposicao
+	FOR each row execute procedure do_reposicao()
+;
