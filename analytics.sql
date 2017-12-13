291b43f15e98179304941880a3ca67189f3a6d4c
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2017-12-13 16:24:27 +0000

    Minor fixes

--- analytics.sql
+++ analytics.sql
@@ -1,21 +1,21 @@
 SELECT *
 FROM (
-	(SELECT T.ano, T.mes, P.categoria, count(T.dateid) as count
+	(SELECT count(T.dateid) as count, T.ano, T.mes, P.categoria
 	FROM facts as F natural join d_produto as P natural join d_tempo as T
 	WHERE P.forn_primario = 267081367
 	GROUP BY T.ano, T.mes, P.categoria)
 UNION
-	(SELECT T.ano, T.mes, null, count(T.dateid) as count
+	(SELECT count(T.dateid) as count, T.ano, T.mes, null
 	FROM facts as F natural join d_produto as P natural join d_tempo as T
 	WHERE P.forn_primario = 267081367
 	GROUP BY T.ano, T.mes)
 UNION
-	(SELECT T.ano, null, null, count(T.dateid) as count
+	(SELECT count(T.dateid) as count, T.ano, null, null
 	FROM facts as F natural join d_produto as P natural join d_tempo as T
 	WHERE P.forn_primario = 267081367
 	GROUP BY T.ano)
 UNION
-	(SELECT null, null, null, count(T.dateid) as count
+	(SELECT count(T.dateid) as count, null, null, null
 	FROM facts as F natural join d_produto as P natural join d_tempo as T
 	WHERE P.forn_primario = 267081367)
 ) as K	
