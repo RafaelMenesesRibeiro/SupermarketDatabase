df92472b534355d2446550b7001b140dade13631
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2017-12-13 19:43:56 +0000

    Fixed asked NIF

--- analytics.sql
+++ analytics.sql
@@ -2,21 +2,21 @@ SELECT *
 FROM (
 	(SELECT count(T.dateid) as count, T.ano, T.mes, P.categoria
 	FROM facts as F natural join d_produto as P natural join d_tempo as T
-	WHERE P.forn_primario = 267081367
+	WHERE P.forn_primario = 123455678
 	GROUP BY T.ano, T.mes, P.categoria)
 UNION
 	(SELECT count(T.dateid) as count, T.ano, T.mes, null
 	FROM facts as F natural join d_produto as P natural join d_tempo as T
-	WHERE P.forn_primario = 267081367
+	WHERE P.forn_primario = 123455678
 	GROUP BY T.ano, T.mes)
 UNION
 	(SELECT count(T.dateid) as count, T.ano, null, null
 	FROM facts as F natural join d_produto as P natural join d_tempo as T
-	WHERE P.forn_primario = 267081367
+	WHERE P.forn_primario = 123455678
 	GROUP BY T.ano)
 UNION
 	(SELECT count(T.dateid) as count, null, null, null
 	FROM facts as F natural join d_produto as P natural join d_tempo as T
-	WHERE P.forn_primario = 267081367)
+	WHERE P.forn_primario = 123455678)
 ) as K	
 ORDER BY count, K.ano, K.mes, K.categoria;
