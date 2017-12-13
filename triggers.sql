08ea06bf530ca45727627b38537da3d1150c7ef5
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2017-12-13 19:53:32 +0000

    Clenaed trigger.sql

--- triggers.sql
+++ triggers.sql
@@ -2,39 +2,31 @@
 --	  fornece_sec para o mesmo produto.
 CREATE OR REPLACE FUNCTION do_produto() RETURNS trigger as $$
 	begin
-		if new.forn_primario in (
-			select nif
-			from fornece_sec
-			where ean = new.ean
-		) then
-			RAISE EXCEPTION 'The primary supplier given is already a secondary supplier for this product. This cannot happen.';
+		if new.forn_primario in ( select nif from fornece_sec where ean = new.ean )
+		then RAISE EXCEPTION 'The primary supplier given is already a secondary supplier for this product. This cannot happen.';
 		end if;
-
 		return NEW;
 	end;
 $$ LANGUAGE 'plpgsql';
 
 CREATE TRIGGER chk_produto
 	before insert or update on produto
-	FOR each row execute procedure do_produto()
-;
+	FOR each row execute procedure do_produto();
 
 -- b) O instante mais recente de reposição tem de ser sempre anterior ou igual 
 --	  à data atual.
 CREATE OR REPLACE FUNCTION do_reposicao() RETURNS trigger as $$
 	begin
-		if new.instante > current_timestamp then
-			RAISE EXCEPTION 'The restocking instant given is in the future. This cannot happen.';
+		if new.instante > current_timestamp
+		then RAISE EXCEPTION 'The restocking instant given is in the future. This cannot happen.';
 		end if;
-
 		return NEW;
 	end;
 $$ LANGUAGE 'plpgsql';
 
 CREATE TRIGGER chk_reposicao
 	before insert or update on reposicao
-	FOR each row execute procedure do_reposicao()
-;
+	FOR each row execute procedure do_reposicao();
 
 ----------------------------------------
 -- Creates the trigger for d_tempo
@@ -50,5 +42,4 @@ $$ LANGUAGE 'plpgsql';
 
 CREATE TRIGGER chk_d_tempo
 	before insert or update on d_tempo
-	FOR each row execute procedure do_d_tempo()
-;
+	FOR each row execute procedure do_d_tempo();
